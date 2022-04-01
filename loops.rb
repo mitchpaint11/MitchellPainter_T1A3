4f47d76d98ce24ebeca4e3f24a38985bc618ee3f
Mitchell Painter <mitchell.painter@hotmail.com>
2022-04-01 16:47:49 +1100

    Cleaning up code for menu option 1

--- loops.rb
+++ loops.rb
@@ -25,23 +25,23 @@ class Dog
 end
 
 loop do 
-    puts "1. For Current Available Jobs"
-    puts "2. For Future/Upcoming Jobs"
-    puts "3. For Past Jobs"
-    puts "4. Exit"
-    puts "Please select (1-4): "
+    puts Rainbow("1 :").orange + Rainbow("Current Available Jobs").blue
+    puts Rainbow("2 :").orange + Rainbow("Future/Upcoming Jobs").blue
+    puts Rainbow("3 :").orange + Rainbow("Past Jobs").blue
+    puts Rainbow("4 :").orange + Rainbow("Exit").blue
+    puts Rainbow("Please select (1-4): ").orange
     choice = gets.chomp
 
     case choice
     when "1"
-        puts "Current Jobs"
+        puts Rainbow("Current Available Jobs:").blue
         puts Rainbow("To get started, please enter your name: ").orange
         name = gets.chomp
         puts "Hello #{name.capitalize}, Are you ready to find a dog to walk?:" + Rainbow(" Y/N ?").orange
-        ready = gets.chomp
+        gets.chomp
         puts "Okay, #{name.capitalize}. Let's choose a dog to walk! "
-        puts Rainbow("return/enter ").orange + "for a list of dogs that need walks today! "
-        dog = gets.chomp
+        puts Rainbow("return/enter ").orange + Rainbow("for a list of dogs that need walks today! ").blue
+        gets.chomp
 
         #Main
         dog1 = Dog.new("1. Buster", "Labrador", 4)
@@ -57,12 +57,12 @@ loop do
         dog4.walk(60)
         dog4.display_walk
 
-        # Note: Juno and Bindi both need 60 min walks and can be walked together!
-        b = "return/enter"
-        puts Rainbow(b).orange
+        # NOTE: Juno and Bindi both need 60 min walks and can be walked together!
+        b = Rainbow("return/enter to continue..").orange
+        puts b
         gets.chomp
         puts "Note: " + Rainbow("Juno").green + " and " + Rainbow("Bindi").green + " both need 60 min walks and can be walked together! "
-        puts Rainbow(b).orange
+        puts b
         gets.chomp
 
         puts "Which dog would you like to walk? "
@@ -76,18 +76,18 @@ loop do
         puts a + Rainbow("Juno & Bindi").green + b + c + Rainbow("5").orange
 
         user_input = gets.chomp.capitalize
-        confirm = 0
+        # confirm = 0
         # conditional statement: if else logic to take different path based on whether the condition was evaluated to true
 
-        a = "return/enter for main menu!"
-        b = "return/enter to continue.."
+        return_main_menu = Rainbow("return/enter for main menu!").orange
+        return_continue = "return/enter to continue.."
         c = "You have chosen to walk "
         d = " Confirm"
-    if  user_input == '1'
+        invalid = Rainbow("Invalid Entry, Try Again!").red
+    if user_input == '1'
         puts c + Rainbow("Buster!").green + d + Rainbow(" Y/N ?").orange
         gets.chomp.to_i
-        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Buster ").green
-        + Rainbow("for a ").blue + Rainbow("30 min ").red + Rainbow("walk.").blue
+        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Buster ").green + Rainbow("for a ").blue + Rainbow("30 min ").red + Rainbow("walk.").blue
         puts Rainbow(b).orange
         gets.chomp
         puts Rainbow("You will be paid ").blue + Rainbow("$20 ").red + Rainbow("for this walk!").blue
@@ -95,8 +95,7 @@ loop do
     elsif user_input == '2'
         puts c + Rainbow("Juno!").green + d + Rainbow(" Y/N ?").orange
         gets.chomp.to_i
-        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Juno ").green + Rainbow("for a ").blue
-        + Rainbow("60 min ").red + Rainbow("walk.").blue
+        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Juno ").green + Rainbow("for a ").blue + Rainbow("60 min ").red + Rainbow("walk.").blue
         puts Rainbow(b).orange
         gets.chomp
         puts Rainbow("You will be paid ").blue + Rainbow("$30 ").red + Rainbow("for this walk!").blue
@@ -104,8 +103,7 @@ loop do
     elsif user_input == '3'
         puts c + Rainbow("Fluffy!").green + d + Rainbow(" Y/N ?").orange
         gets.chomp.to_i
-        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Fluffy ").green
-        + Rainbow("for a ").blue + Rainbow("45 min ").red + Rainbow("walk.").blue
+        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Fluffy ").green + Rainbow("for a ").blue + Rainbow("45 min ").red + Rainbow("walk.").blue
         puts Rainbow(b).orange
         gets.chomp
         puts Rainbow("You will be paid ").blue + Rainbow("$25 ").red + Rainbow("for this walk!").blue
@@ -113,8 +111,7 @@ loop do
     elsif user_input == '4'
         puts c + Rainbow("Bindi!").green + d + Rainbow(" Y/N ?").orange
         gets.chomp.to_i
-        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Bindi ").green + Rainbow("for a ").blue
-        + Rainbow("60 min ").red + Rainbow("walk.").blue
+        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Bindi ").green + Rainbow("for a ").blue + Rainbow("60 min ").red + Rainbow("walk.").blue
         puts Rainbow(b).orange
         gets.chomp
         puts Rainbow("You will be paid ").blue + Rainbow("$30 ").red + Rainbow("for this walk!").blue
@@ -122,14 +119,13 @@ loop do
     elsif user_input == '5'
         puts c + Rainbow("Juno & Bindi").green + d + Rainbow(" Y/N ?").orange
         gets.chomp.to_i
-        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Juno & Bindi ").green
-        + Rainbow("for a ").blue + Rainbow("60 min ").red + Rainbow("walk.").blue
+        puts Rainbow("Great, You have confirmed you will take ").blue + Rainbow("Juno & Bindi ").green + Rainbow("for a ").blue + Rainbow("60 min ").red + Rainbow("walk.").blue
         puts Rainbow(b).orange
         gets.chomp
         puts Rainbow("You will be paid ").blue + Rainbow("$40 ").red + Rainbow("for this walk!").blue
         puts Rainbow(a).orange
     else
-        puts "Invalid Entry, Try Again !"
+        puts invalid
     end
         gets.chomp
 
@@ -138,10 +134,10 @@ loop do
     when "3"
         puts "Past Jobs"
     when "4"
-        puts "Exit!"
+        puts Rainbow("Thank You, Have A Nice Day!").orange
         break
     else
-        puts "Invalid choice, Try again!"
+        puts invalid
     end
 end
 
