6f3c85b314950256ff95a2256f12dc1e26d47fee
Mitchell Painter <mitchell.painter@hotmail.com>
2022-04-02 17:24:14 +1100

    added RainbowGem to option 2

--- main.rb
+++ main.rb
@@ -1,15 +1,18 @@
 require 'rainbow'
 
+# Class Dog
 class Dog
     attr_reader :age, :breed
-    attr_accessor :name
+    attr_accessor :name, :date, :future_walk
 
-    def initialize(name, breed, age)
+    def initialize(name, breed, age, future_walk, date)
         @name = name
         @breed = breed
         @age = age
+        @future_walk = Rainbow(future_walk).blue
+        @date = Rainbow(date).green
         @walk = []
-        @future_walk = []
+        # @future_walk = []
     end
 
     def walk(time_in_mins)
@@ -20,20 +23,29 @@ class Dog
         return @walk.sum
     end
 
-    def future_walk(time_of_day)
-        @future_walk << time_of_day
-    end
+    # def future_walk(time_of_day)
+    #     @future_walk << time_of_day
+    # end
 
-    def future_walk_time
-        return @future_walk.sum
-    end
+    # def future_walk_time
+    #     return @future_walk.sum
+    # end
+
+    # def date(day_month_year)
+    #     @date << day_month_year
+    # end
+
+    # def date_day_month_year
+    #     return @date.sum
+    # end
 
     def display_walk
     puts Rainbow("#{@name} ").green + "is a " + Rainbow("#{@breed}").red + ", is #{@age} years old and needs a #{walk_time} min walk. "
     end
+
     def display_future_walk
     a = ","
-    puts Rainbow(" #{@name}").green + a + Rainbow(" #{@breed}").red + a + " #{@age} yrs old" + a + " #{walk_time} min walk" + a + "#{future_walk_time}"
+    puts Rainbow(" #{@name}").green + a + Rainbow(" #{@breed}").red + a + " #{@age} yrs old" + a + Rainbow("#{walk_time} min walk").green  + a + " #{@future_walk}" + a + " #{@date}"
     end
 end
 
@@ -143,46 +155,46 @@ loop do
 
     when "2"
         puts Rainbow("Future/Upcoming Jobs").blue
-        puts " Name:    Breed:   Age:   Walk-duration:  Time/24hrs: "
-        dog1 = Dog.new("1. Cooper", "Labrador", 4)
+        puts " Name:    Breed:   Age:   Walk-duration:  Time/24hrs:  Date:"
+        dog1 = Dog.new("1. Cooper", "Labrador", 4, "12:00", "03/05/2022")
         dog1.walk(30)
-        dog1.future_walk(1200)
+        # dog1.future_walk(1200)
         dog1.display_future_walk
-        dog2 = Dog.new("2. Koda", "Border Collie", 2)
+        dog2 = Dog.new("2. Koda", "Border Collie", 2, "11:00", "08/05/2022")
         dog2.walk(60)
-        dog2.future_walk(1100)
+        # dog2.future_walk(1100)
         dog2.display_future_walk
-        dog3 = Dog.new("3. Sandy", "Groodle", 3)
+        dog3 = Dog.new("3. Sandy", "Groodle", 3, "13:00", "12/05/2022")
         dog3.walk(45)
-        dog3.future_walk(1300)
+        # dog3.future_walk(1300)
         dog3.display_future_walk
-        dog4 = Dog.new("4. Bailey", "Springer Spanial", 2)
+        dog4 = Dog.new("4. Bailey", "Springer Spanial", 2, "13:30", "28/07/2022")
         dog4.walk(60)
-        dog4.future_walk(1300)
+        # dog4.future_walk(1300)
         dog4.display_future_walk
-        dog5 = Dog.new("5. Harley", "Australian Shepherd", 5)
+        dog5 = Dog.new("5. Harley", "Australian Shepherd", 5, "15:00", "21/06/2022")
         dog5.walk(60)
-        dog5.future_walk(1500)
+        # dog5.future_walk(1500)
         dog5.display_future_walk
-        dog6 = Dog.new("6. Thor", "Australian Cattle Dog", 2)
+        dog6 = Dog.new("6. Thor", "Australian Cattle Dog", 2, "11:00", "17/06/2022")
         dog6.walk(60)
-        dog6.future_walk(1100)
+        # dog6.future_walk(1100)
         dog6.display_future_walk
-        dog7 = Dog.new("7. Teddy", "Groodle", 3)
+        dog7 = Dog.new("7. Teddy", "Groodle", 3, "12:30", "13/05/2022")
         dog7.walk(45)
-        dog7.future_walk(1200)
+        # dog7.future_walk(1200)
         dog7.display_future_walk
-        dog8 = Dog.new("8. Benji", "Border Collie", 6)
+        dog8 = Dog.new("8. Benji", "Border Collie", 6, "16:00", "20/06/2022")
         dog8.walk(30)
-        dog8.future_walk(1600)
+        # dog8.future_walk(1600)
         dog8.display_future_walk
-        dog9 = Dog.new("9. Piper", "Springer Spanial", 4)
+        dog9 = Dog.new("9. Piper", "Springer Spanial", 4, "17:00", "05/05/2022")
         dog9.walk(30)
-        dog9.future_walk(1700)
+        # dog9.future_walk(1700)
         dog9.display_future_walk
-        dog10 = Dog.new("10. Peanut", "Beagle", 1)
+        dog10 = Dog.new("10. Peanut", "Beagle", 1, "16:00", "14/05/2022")
         dog10.walk(30)
-        dog10.future_walk(1200)
+        # dog10.future_walk(1200)
         dog10.display_future_walk
         puts Rainbow("return/enter for main menu!").orange
         gets.chomp
